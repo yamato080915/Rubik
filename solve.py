a04649421ecb5e66e52dd44aba54e64228785aac
Yamat <145124662+yamato080915@users.noreply.github.com>
2025-06-30 18:06:17 +0900

    solve_bfs

--- solve.py
+++ solve.py
@@ -1,7 +1,8 @@
 import numpy as np
+from collections import deque
 from main import *
 
-MAXDEPTH = 5
+MAXDEPTH = 6
 
 def is_solved(cube:np.ndarray=reset()):
 	return np.array_equal(cube, reset())
@@ -28,7 +29,35 @@ def solve_dfs(cube:np.ndarray, depth=0, moves=[]):
 			else:
 				solve_dfs(moved, depth+1, moves_)
 
-string = " ".join(scramble(5))
+def solve_bfs(input:np.ndarray):
+	queue = deque()
+	history = {}
+
+	byte = input.tobytes()
+	queue.append( (input, [], 0) )
+	history[byte] = 0
+
+	while queue:
+		cube, moves, depth = queue.popleft()
+
+		if is_solved(cube):
+			print(" ".join([f"{faceNames[x[0]]}{movetype[x[1]]}" for x in moves]))
+			return
+		if depth>=MAXDEPTH:
+			continue
+		for i in range(18):
+			if depth==0 or moves[-1][0] != i//3:
+				moved = cube.copy()
+				mov(moved, faceNames[i//3], i%3==1)
+				if i%3==2:
+					mov(moved, faceNames[i//3])
+				byte = moved.tobytes()
+				if byte in history and history[byte] <= depth + 1:
+					continue
+				history[byte] = depth + 1
+				queue.append( (moved, moves + [[i//3, i%3]], depth + 1) )
+
+string = " ".join(scramble(6))
 print("scramble =", string)
 cube = move(string=string)
-solve_dfs(cube)
+solve_bfs(cube)
